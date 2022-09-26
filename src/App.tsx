7c94c579369ff9bc5d0ffba3f43df175e5315c84
Edwin Pinchao <miguepinchao1992@gmail.com>
2022-09-26 16:09:05 -0500

    "Instalaciòn de styled-components, en el package.json Configuracion vite.config.ts .. path.resolve install pnpm i @types/node *  agregar utilizacion de informacion , informacion que se muestra HOME * creacion de home-title.styled.ts"

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css';
 import { Home } from './pages/home';
+import store from './redux/store';
+import { useSelector } from 'react-redux';
+import { AppStore } from './redux/store';
 
 function App() {
-return <Home/>;  
+   const user= useSelector((state: AppStore) => state.user)
+   return (<>
+    <div>
+    {JSON.stringify(user)}
+    <Home/>
+    </div>
+    </>);  
 }
 
 export default App
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
-
+import { createUser } from '@/redux/slices/user.slice';
+import { Layout } from '@/styled-components';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { HomeTitle } from './styled-components';
 export default function Home() {
-  return (
-    <div> Hola Home</div>
+    const dispatch = useDispatch();
+    
+    const dispatchAction = ()=> {
+             dispatch(createUser({name:'jhon Doe', id:'1'}));
+       
+    }
+
+    return (
+    <Layout>
+        <button onClick={dispatchAction}>dispatchAction</button>
+        <HomeTitle> Hola Home</HomeTitle>
+    </Layout>
   )
 }
