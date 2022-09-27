a87295c7bebe60167ef4c5f978eadfd2788a337a
Edwin Pinchao <miguepinchao1992@gmail.com>
2022-09-26 22:16:42 -0500

    "Creacion de un boton create button. Clean Código separación Home.tsx CreateHomebutton.tsx "

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,20 +1,11 @@
-import { createUser } from '@/redux/slices/user.slice';
 import { Layout } from '@/styled-components';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { CreateHomeButton } from './components';
 import { HomeTitle } from './styled-components';
 export default function Home() {
-    const dispatch = useDispatch();
-    
-    const dispatchAction = ()=> {
-             dispatch(createUser({name:'jhon Doe', id:'1'}));
-       
-    }
-
     return (
     <Layout>
-        <button onClick={dispatchAction}>dispatchAction</button>
         <HomeTitle> Hola Home</HomeTitle>
+        <CreateHomeButton/>
     </Layout>
   )
 }
